25724839af293d403f4c3dd9bc1f171021aa9759
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-02-27 18:57:37 -0300

    feature update ---

--- tindev-frontend/src/services/api.js
+++ tindev-frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-let apiURL;
-if (process.env.NODE_ENV === 'development') {
-  apiURL = 'http://localhost:3333';
-} else if (process.env.NODE_ENV === 'production') {
-  apiURL = process.env.REACT_APP_API_URL;
-}
+ let apiURL = 'https://tindev-app.herokuapp.com';
+// if ('development' === 'development') {
+//   apiURL = 'https://tindev-app.herokuapp.com';
+// } else if ( 'development' === 'production') {
+//   apiURL = process.env.REACT_APP_API_URL;
+// }
 
 const api = axios.create({
   baseURL: apiURL,
